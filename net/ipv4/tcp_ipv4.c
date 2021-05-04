8548ce97ce4c09e0c86aa1a6eea7da9f08e1894d
Panchajanya1999 <panchajanya@azure-dev.live>
2021-05-05 07:11:59 +0800

    net: ipv4: Reduce TCP performance spikes
    Turning off TCP timestamps re4duces performance spikes related to
    timestamp generation.
    Making the node read-only prevents any unwanted writes to the node.
    
    This tuning is suggested by RedHat[1].
    
    [1] https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/tuning_guide/reduce_tcp_performance_spikes
    
    Change-Id: I0b8892e001deef23c67b34af9be5673e74a0eb65
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2513,7 +2513,7 @@ static int __net_init tcp_sk_init(struct net *net)
 	net->ipv4.sysctl_max_syn_backlog = max(128, cnt / 256);
 	net->ipv4.sysctl_tcp_sack = 1;
 	net->ipv4.sysctl_tcp_window_scaling = 1;
-	net->ipv4.sysctl_tcp_timestamps = 1;
+	net->ipv4.sysctl_tcp_timestamps = 0;
 
 	#ifdef VENDOR_EDIT
 	//Hao.Peng@PSW.CN.WiFi.Network.login.1854960, 2019/03/30,
